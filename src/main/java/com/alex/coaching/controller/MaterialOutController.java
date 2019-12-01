921547c13c2663615c1e1affb0a4de370ad8f84c
Administrator <alex_huangjiajian@163.com>
2019-12-01 23:55:52 +0800

    做到提交表单部分

--- src/main/java/com/alex/coaching/controller/MaterialOutController.java
+++ src/main/java/com/alex/coaching/controller/MaterialOutController.java
@@ -2,6 +2,7 @@ package com.alex.coaching.controller;
 
 import com.alex.coaching.model.MaterialOut;
 import com.alex.coaching.service.MaterialOutService;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -43,6 +47,37 @@ public class MaterialOutController {
         jsonObject.put("msg","");
         jsonObject.put("count",count);
         jsonObject.put("data",materialOutList.toArray());
+        return jsonObject;
+    }
+
+    @RequestMapping("/add")
+    public JSONObject addMaterialOut(String record){
+        JSONObject jsonObject = new JSONObject();
+        if(record.equals("")||record==null){
+            jsonObject.put("code",-1);
+            jsonObject.put("msg","参数为空！");
+            return jsonObject;
+        }
+
+        JSONObject recordJson = JSONObject.parseObject(record);
+        String lotNumArr[] = recordJson.getString("lotNumberList").split(",") ;
+        String outNumArr[] = recordJson.getString("outnumList").split(",") ;
+
+        if(lotNumArr.length!=outNumArr.length){
+            //批号数量没有对应上
+            jsonObject.put("code",-1);
+            jsonObject.put("msg","批号数量不对应！");
+            return jsonObject;
+        }
+        for (int i =0;i<lotNumArr.length;i++){
+          MaterialOut out = new MaterialOut();
+          out.setLotNumber(lotNumArr[i]);
+          out.setMaterialid(recordJson.getInteger("mId"));
+          out.setName(recordJson.getString("name"));
+          out.setOutNum(recordJson.getDouble("outNum"));
+          out.setSpecifications(recordJson.getString("specifications"));
+          out.setOutTime(new Date());
+        }
         return jsonObject;
     }
 
