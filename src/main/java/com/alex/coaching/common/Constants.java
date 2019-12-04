ac444bc4b53f71c726227c25dd17b45c7f0fcbbc
AlexHuangjiajian <42435872+AlexHuangjiajian@users.noreply.github.com>
2019-12-04 18:02:06 +0800

    下一步: 1、outList页面获取所有选择行，渲染到word里面 问题: 1、word表头标题没有动态渲染到 2、编辑页面select没办法选中值

---
+++ src/main/java/com/alex/coaching/common/Constants.java
@@ -0,0 +1,16 @@
+package com.alex.coaching.common;
+
+/**
+ * @Author: alex_huang
+ * @Description:
+ * @Date: Create in 17:14 2019/12/4
+ */
+public class Constants {
+
+    //公司名称
+    public static final String COMPANY_NAME = "广州辰星教育有限公司";
+    //渲染表格后临时存放位置
+    public static final String EXCEL_TEMP = Constants.class.getResource("/").getPath()+"static/ExcelTemp/";
+    //出仓单模板路径
+    public static final String OUTEXCEL_PATH =Constants.class.getResource("/").getPath()+"static/Excel/outExcel.docx";
+}
--- src/main/java/com/alex/coaching/controller/MaterialOutController.java
+++ src/main/java/com/alex/coaching/controller/MaterialOutController.java
@@ -1,5 +1,8 @@
 package com.alex.coaching.controller;
 
+import com.alex.coaching.common.Constants;
+import com.alex.coaching.common.FileUtil;
+import com.alex.coaching.common.MapObjUtil;
 import com.alex.coaching.model.MaterialOut;
 import com.alex.coaching.service.MaterialOutService;
 import com.alibaba.fastjson.JSON;
@@ -12,10 +15,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.Date;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * @Author: alex_huang
@@ -123,6 +128,57 @@ public class MaterialOutController {
         jsonObject.put("materialOut", JSONObject.toJSON(out));
         jsonObject.put("msg","获取成功");
         jsonObject.put("code",1);
+        return jsonObject;
+    }
+
+    @RequestMapping("/printWord")
+    public JSONObject printWord(String data, HttpServletRequest request, HttpServletResponse response)throws Exception{
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code",-1);
+        if(data.equals("")||data==null){
+            jsonObject.put("msg","未选择出仓纪录");
+            return jsonObject;
+        }
+        List<MaterialOut> jsonArray = JSONObject.parseArray(data,MaterialOut.class);
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (int i=0;i<jsonArray.size();i++){
+            MaterialOut out = jsonArray.get(i);
+
+            //TODO: 此处map数据需要做处理,去除不需要的字段，加上表格需要的字段
+            list.add(MapObjUtil.object2Map(out));
+        }
+
+        Map<String,Object> textMap=new HashMap<>();
+        textMap.put("companyName", Constants.COMPANY_NAME);
+        textMap.put("y",Calendar.getInstance().get(Calendar.YEAR));
+        textMap.put("m",Calendar.getInstance().get(Calendar.MONTH)+1);
+        textMap.put("d",Calendar.getInstance().get(Calendar.DATE));
+
+        // 模板文件输入输出地址
+        //String filePath =WordReporter.class.getResource("/").getPath()+"static/Excel/test.docx";
+        String filePath = Constants.OUTEXCEL_PATH;
+        System.out.println(filePath);
+        //输出文件位置
+        String longTime = new SimpleDateFormat("yyyy-MM-dd/HH:MM:SS").format(new Date());
+        String excelName = "outExcel"+longTime+".docx";
+        String outPath = Constants.EXCEL_TEMP+excelName;
+        //渲染数据
+        WordReporter wordReporter = new WordReporter();
+        wordReporter.setTempLocalPath(filePath);    //设置模板的路径
+        wordReporter.init();            //初始化工具类
+        wordReporter.export(list,null,null,0,textMap);   //写入相关数据
+        wordReporter.generate(outPath);   //导出到目标文档
+
+        //浏览器下载
+        File file = new File(outPath);
+        InputStream inputStream = new FileInputStream(file);
+        FileUtil.doExport(excelName,outPath,request,response);
+
+        //最后删除文件
+        file.delete();
+        jsonObject.put("code",0);
+        jsonObject.put("msg","导出成功");
+
         return jsonObject;
     }
 }
